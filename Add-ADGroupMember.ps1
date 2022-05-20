59fd2c8310ad87860c062c148def31331351bd69
ERDEM UÇAK <52521399+erdemucak@users.noreply.github.com>
2022-05-20 08:04:28 +0300

    Add files via upload

---
+++ Add-ADGroupMember.ps1
@@ -0,0 +1,6 @@
+﻿# Add to Active Directory Groups
+Add-ADGroupMember -Identity WSM -Members "DTEUCAK"
+
+
+#Add to Multiple Users active Directory Groups
+Add-ADGroupMember -Identity "CC CTX Digifarm ITTP_ITTPTEST" -Members "User1","User2","User3"
